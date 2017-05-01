838e793c6b38d008cd2bcf11ed4830dc28deb81f
Lenz Weber <mail@lenzw.de>
2017-05-01 22:47:27 +0200

    fixup

--- build_win.bat
+++ build_win.bat
@@ -3,6 +3,7 @@ REM ./lib should contain files from C:\Program Files (x86)\GnuPG\lib, with exten
 REM ./ should contain dll files from C:\Program Files (x86)\GnuPG\bin (libassuan-0.dll  libgpg-error-0.dll  libgpgme-11.dll)
 
 set PATH=%PATH%;C:\Go\bin
+set PATH=%PATH%;C:\mingw\bin
 set PATH=%PATH%;C:\mingw\mingw32\bin
 set GOPATH=%~dp0
 
