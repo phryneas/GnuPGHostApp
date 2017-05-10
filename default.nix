77888c280c8cc4b49269d44839edbf45f695820f
Lenz Weber <lenz.weber@mayflower.de>
2017-05-10 15:38:55 +0200

    nixos

---
+++ default.nix
@@ -0,0 +1,18 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "gpgmeHostApp";
+
+  buildInputs = [
+	go
+	godep
+	gpgme
+  ];
+
+shellHook =
+  ''
+export GOROOT=$(cd $(dirname $(which go)); cd ../share/go; pwd)
+export GOPATH=$(pwd)
+export GNUPGHOME=$(cd test_keys/GPGHOME/; pwd)
+  '';
+}
